29d6edbd944e4f8b239b145b02246ea3bb4cd1ab
KevJoss <kelth.joss53@gmail.com>
2025-09-09 11:57:53 -0500

    Initial commit: FastAPI file and docker container for the server

---
+++ HTTP/app.py
@@ -0,0 +1,49 @@
+ # app.py
+# THIS CODES CREATE MY SERVER RESPONSES TO GET METHODS AND 
+# SPECIALIZED METHODS DEFINED BY ME
+
+from fastapi import FastAPI, Response, Request
+
+# This is your main API object, used to define routes and run the server.
+app = FastAPI()
+
+# @app.get("/") tells FastAPI: 
+# "Register this function to handle GET requests to the root path (/)"
+
+@app.get("/")
+
+# When someone visits http://localhost:8000/ with a GET request, this function executes
+# It returns a Python dictionary that converts to JSON file to headers.
+def read_root():
+    return {"message": "Hello, GET request!"}
+
+# POST requests are typically used to send data to the server (unlike GET which just retrieves data)
+# When a client sends a POST request to http://localhost:8000/submit, this function runs
+
+@app.post("/submit")
+
+# 2. FastAPI automatically:
+#       Reads the JSON from the request body
+#       Converts it to a Python dictionary
+#       Passes it as the data parameter to your function
+
+# Parameter: data: dict means this function expects to receive a dictionary
+def submit_data(data: dict):
+    return {"received": data}
+
+
+# Decorator that handles GET requests to the /cookie URL
+# When someone visits http://localhost:8000/cookie, this function executes
+
+
+@app.get("/cookie")
+
+# response: Response - This gives you access to modify the HTTP response
+def set_cookie(response: Response):
+    
+    # This cookie will be stored in the browser and sent back with future requests
+    response.set_cookie(key="user", value="student123")
+    
+    # Returns a JSON response confirming the cookie was set
+    # The browser receives both the JSON message AND the cookie
+    return {"message": "Cookie set!"}
--- tcp-lab/client/client.py
+++ tcp-lab/client/client.py
@@ -1,10 +1,21 @@
 import socket
 
-HOST = '<EC2-public-IP>'
+# Give the AWS EC2 instance's public IP address here and the port number
+# where the server is listening
+HOST = '13.58.165.247'
 PORT = 12345
 
+# Create a TCP socket and connect to the server
 s = socket.socket()
+
+# Initiates a conection from the client to the server at the specified HOST and PORT
 s.connect((HOST, PORT))
+
+# Waits to receive up to 1024 bytes of data from the server.
 data = s.recv(1024)
+
+# Decodes the received bytes into a string and prints it.
 print("Received:", data.decode())
+
+# Closes the socket connection.
 s.close()
