9ea14aff6cbae03807e3f1765fdd8db7d96c1416
KevJoss <kelth.joss53@gmail.com>
2025-09-26 11:20:17 -0500

    Handling errors with Sockets - Improve calculator

--- Sockets/calculator_server.py
+++ Sockets/calculator_server.py
@@ -74,24 +74,29 @@ while True:
     
     # Shows the connection time with Year-Month-Day | Hour-Minutes-Seconds
     time.strftime("%Y-%m-%d %H:%M:%S")
+    logger.info('Conection start!')
     logger.info(f'Connection START time: {time.strftime("%Y-%m-%d %H:%M:%S")}')
     
     # Client information
-    logger.info('Conection start!')
     logger.info(f'The clien IP is {addr[0]}')
     logger.info(f'The client port is {addr[1]}\n')
     
     # Data information
     data = conn.recv(1024).decode()                             # Receive up to 1024 bytes and decode from bytes to string
-    logger.info(f'Received data from the client: {json.loads(data)}\n')
+    logger.info(f'Received data from the client: {json.loads(data)}')
+    logger.info(f'OPERATION: {json.loads(data['operation'])}')
+    logger.info(f'VALUE_a: {json.loads(data['a'])}')
+    logger.info(f'VALUE_b: {json.loads(data['b'])}\n')
     
     # Time processing to data
     start = time.time()
     response = handle_request(data)                             # Process the request and get response dictionary
     logger.info(f'Response by the server to client: {response}')
+    logger.info(f'RESULT: {response['result']}')
+    logger.info(f'CODE: {response['code']}')
     end = time.time()
     total_time = abs(start - end)
-    logger.info(f'Time to process data was: {total_time:.4f} seconds\n')
+    logger.info(f'Time to process data was: {total_time:.8f} seconds\n')
     
     conn.send(json.dumps(response).encode())        # Convert/serialize response to JSON string, encode to bytes, and send
     
